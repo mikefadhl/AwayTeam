8d68cae279f753cc0e4b357c26d289b32a17d7da
sivakumar govindasamy <go.sivakumar@gmail.com>
2015-08-19 17:12:50 -0700

    Ship - Get & Give energy between shield logic added

--- src/test/java/com/test/awayteam/ShipTest.java
+++ src/test/java/com/test/awayteam/ShipTest.java
@@ -1,7 +1,10 @@
 package com.test.awayteam;
 
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -14,10 +17,12 @@ public class ShipTest {
     private Ship ship;
     private Shield shield;
 
+    private final String shieldKey = "shield";
+
     @Before
     public void setUp() throws Exception {
         ship = new Ship();
-        shield = (Shield) ship.getSubsystems().get("shield");
+        shield = (Shield) ship.getSubsystems().get(shieldKey);
     }
 
     @Test
@@ -53,5 +58,48 @@ public class ShipTest {
         assertTrue("Subsystem is damaged", ship.getSubsystems().get("shield").isDamaged());
     }
 
+    @Test
+    public void testGetEnergyFromShield(){
+        ship.getEnergyFromShield(1000);
+        assertEquals(5000, ship.getStrength());
+        assertEquals(3000, shield.getStrength());
+    }
 
+    @Test
+    public void testGetEnergyFromShieldExceedCurrentEnergyLevel(){
+        ship.getEnergyFromShield(4001);
+        assertEquals(8000, ship.getStrength());
+        assertEquals(0,shield.getStrength());
+    }
+
+    @Test
+    public void testGetEnergyFromShieldNegetiveEnergyLevel(){
+        ship.getEnergyFromShield(-1);
+        assertEquals(4000, ship.getStrength());
+        assertEquals(4000,shield.getStrength());
+    }
+
+    // give
+    @Test
+    public void testGiveEnergyToShield(){
+        ship.giveEnergyToShield(1000);
+        assertEquals(3000, ship.getStrength());
+        assertEquals(5000, shield.getStrength());
+    }
+
+    @Test
+    public void testGiveEnergyToShieldExceedCurrentEnergyLevel(){
+        Ship shipLocal = new Ship(10000);
+        shipLocal.giveEnergyToShield(6001);
+        Shield shieldLocal = (Shield) shipLocal.getSubsystems().get(shieldKey);
+        assertEquals(4000, shipLocal.getStrength());
+        assertEquals(10000,shieldLocal.getStrength());
+    }
+
+    @Test
+    public void testGiveEnergyToShieldNegetiveCurrentEnergyLevel(){
+        ship.giveEnergyToShield(-1);
+        assertEquals(4000, ship.getStrength());
+        assertEquals(4000,shield.getStrength());
+    }
 }
